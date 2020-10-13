bcdc26babb90867274054421d69b57168ac715b0
Aleh <inf13bfc@gmail.com>
2020-10-13 19:35:21 +0300

    Add benchmark

---
+++ Vostronia.Benchmark.Runnner/GenericTest.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vostronia.Benchmark.Runnner
+{
+    public class GenericTest
+    {
+        [Benchmark]
+        public void Run()
+        {
+            DoStuff();
+        }
+
+        private void DoStuff()
+        {
+            var list = new List<int>();
+
+            for (var i = 0; i <= 100000; i++)
+            {
+                list.Add(i);
+            }
+        }
+    }
+}
---
+++ Vostronia.Benchmark.Runnner/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace Vostronia.Benchmark.Runnner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<GenericTest>();
+        }
+    }
+}
